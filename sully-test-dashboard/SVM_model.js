294430c9a5dd76d25d8687a0f3efee21a5d9b9ca
sullivanshave@gmail.com <sullivanshave@gmail.com>
2023-12-12 17:23:40 -0600

    further changes to html and css

---
+++ sully-test-dashboard/SVM_model.js
@@ -0,0 +1,237 @@
+// Sample player data
+const playerData = [
+    {name: 'Jimmy Butler', score: 1},
+    {name:'Joel Embiid', score: 1},
+    {name:'De’Aaron Fox', score: 1},
+    {name:'Devin Booker', score: 1},
+    {name:'Luka Doncic',score: 1},
+    {name:'Nikola Jokic', score: 1},
+    {name:'Donovan Mitchell', score: 1},
+    {name:'Giannis Antetokounmpo', score: 1},
+    {name:'Shai Gilgeous-Alexander', score: 1},
+    {name:'Ja Morant', score: 1},
+    {name:'Stephen Curry', score: .6},
+    {name:'Jalen Brunson', score: .5},
+    {name:'Jayson Tatum', score: .3},
+    {name:'Domantas Sabonis', score: .3}
+];
+
+// Sample images path
+const imagePathMap = {
+    'Jimmy Butler': 'NBA Pictures/Jimmy_Butler_Picture.avif',
+    'Joel Embiid': 'NBA Pictures/Joel_Embiid_Picture.jpg',
+    'De’Aaron Fox': 'NBA Pictures/De’Aaron_Fox_Picture.webp',
+    'Devin Booker': 'NBA Pictures/Devin_Booker_Picture.jpg',
+    'Luka Doncic': 'NBA Pictures/Luka_Doncic_Picture.webp',
+    'Nikola Jokic': 'NBA Pictures/Nikola_Jokic_Picture.jpg',
+    'Donovan Mitchell': 'NBA Pictures/Donovan_Mitchell_Picture.jpg',
+    'Giannis Antetokounmpo': 'NBA Pictures/Giannis_Antetokounmpo_Picture.jpg',
+    'Shai Gilgeous-Alexander': 'NBA Pictures/Shai_Gilgeous-Alexander_Picture.webp',
+    'Ja Morant': 'NBA Pictures/Ja_Morant_Picture.webp',
+    'Stephen Curry': 'NBA Pictures/Stephen_Curry_Picture.jpg',
+    'Jalen Brunson': 'NBA Pictures/Jalen_Brunson_Picture.jpg',
+    'Jayson Tatum': 'NBA Pictures/Jayson_Tatum_Picture.avif',
+    'Domantas Sabonis': 'NBA Pictures/Domantas_Sabonis_Picture.jpg'
+   
+};
+
+// Set up the chart
+const diameter = 600;
+
+const bubbleSVM = d3.pack()
+    .size([diameter, diameter])
+    .padding(1.5);
+
+// Create the SVG element
+const svgSVM = d3.select("#bubble-chart-container")
+    .append("svg")
+    .attr("width", diameter)
+    .attr("height", diameter)
+    .attr("class", "bubble");
+
+d3.csv("Datasets/all_nba_seasons_revised.csv").then(function (statsData) {
+    // Convert statsData to a map for easy lookup
+    const statsMap = new Map(statsData.map(entry => [entry.Player, entry]));
+
+    // Generate the chart
+    const root = d3.hierarchy({ children: playerData })
+    .sum(function (d) { return d.score || 1; });
+
+    const nodes = bubbleSVM(root).descendants(); // Change 'bubble' to 'bubbleSVM'
+
+    const node = svgSVM.selectAll(".node") // Change 'svg' to 'svgSVM'
+        .data(nodes)
+        .enter()
+        .filter(d => !d.children)
+        .append("g")
+        .attr("class", "node")
+        .attr("transform", function (d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        });
+
+    node.append("circle")
+        .attr("r", function (d) { return d.r; })
+        .style("fill", "lightblue")
+        .on("click", function (d) {
+            showPlayerImage(d.data.name);
+        });
+
+    node.append("text")
+        .attr("dy", ".3em")
+        .style("text-anchor", "middle")
+        .text(function (d) { return d.data.name; })
+        .attr("class", "player-name")
+        .on("click", function (d) {
+            showPlayerImage(d.data.name);
+        });
+
+    function showPlayerImage(playerName) {
+        const modal = document.getElementById('player-modal');
+        const modalImage = document.getElementById('modal-image');
+        const modalStats = document.getElementById('modal-stats');
+        
+        // Retrieve player data from the map
+        const playerData = statsMap.get(playerName);
+
+        if (playerData) {
+            const imagePath = imagePathMap[playerName];
+            modalImage.src = imagePath;
+            
+            
+            const formattedStats = Object.entries(playerData)
+                .map(([key, value]) => `${key}: ${value}`)
+                .join('\n');
+            
+            // Display player stats
+            modalStats.textContent = `Stats:\n${formattedStats}`;
+        } else {
+            modalStats.textContent = 'Player data not available';
+        }
+
+        modal.style.display = 'block';
+
+        // Close modal on click outside the image
+        modal.onclick = function () {
+            modal.style.display = 'none';
+        };
+    }
+});
+
+// Sample player data
+const keyshawnsPlayerData = [
+    {name: 'Jimmy Butler', score: 1},
+    {name:'Joel Embiid', score: .3},
+    {name:'De’Aaron Fox', score: 1},
+    {name:'Devin Booker', score: 1},
+    {name:'Luka Doncic',score: 1},
+    {name:'Nikola Jokic', score: .5},
+    {name:'Donovan Mitchell', score: 1},
+    {name:'Giannis Antetokounmpo', score: .2},
+    {name:'Shai Gilgeous-Alexander', score: 1},
+    {name:'Ja Morant', score: 1},
+    {name:'Stephen Curry', score: .6},
+    {name:'Jalen Brunson', score: .5},
+    {name:'Jayson Tatum', score: .3},
+    {name:'Domantas Sabonis', score: .3}
+];
+
+// Sample images path
+const keyshawnsImagePathMap = {
+    'Jimmy Butler': 'NBA Pictures/Jimmy_Butler_Picture.avif',
+    'Joel Embiid': 'NBA Pictures/Joel_Embiid_Picture.jpg',
+    'De’Aaron Fox': 'NBA Pictures/De’Aaron_Fox_Picture.webp',
+    'Devin Booker': 'NBA Pictures/Devin_Booker_Picture.jpg',
+    'Luka Doncic': 'NBA Pictures/Luka_Doncic_Picture.webp',
+    'Nikola Jokic': 'NBA Pictures/Nikola_Jokic_Picture.jpg',
+    'Donovan Mitchell': 'NBA Pictures/Donovan_Mitchell_Picture.jpg',
+    'Giannis Antetokounmpo': 'NBA Pictures/Giannis_Antetokounmpo_Picture.jpg',
+    'Shai Gilgeous-Alexander': 'NBA Pictures/Shai_Gilgeous-Alexander_Picture.webp',
+    'Ja Morant': 'NBA Pictures/Ja_Morant_Picture.webp',
+    'Stephen Curry': 'NBA Pictures/Stephen_Curry_Picture.jpg',
+    'Jalen Brunson': 'NBA Pictures/Jalen_Brunson_Picture.jpg',
+    'Jayson Tatum': 'NBA Pictures/Jayson_Tatum_Picture.avif',
+    'Domantas Sabonis': 'NBA Pictures/Domantas_Sabonis_Picture.jpg'
+   
+};
+
+// Set up the chart
+const keyshawnsDiameter = 600;
+
+const keyshawnsBubble = d3.pack()
+    .size([keyshawnsDiameter, keyshawnsDiameter])
+    .padding(1.5);
+
+// Create the SVG element for Keyshawn's Model
+const keyshawnsSvg = d3.select("#keyshawns-bubble-chart-container")
+    .append("svg")
+    .attr("width", keyshawnsDiameter)
+    .attr("height", keyshawnsDiameter)
+    .attr("class", "bubble");
+
+d3.csv("Path_to_Keyshawns_CSV_File.csv").then(function (keyshawnsStatsData) {
+    
+    const keyshawnsStatsMap = new Map(keyshawnsStatsData.map(entry => [entry.Player, entry]));
+
+    // Generate Keyshawn's Model
+    const keyshawnsRoot = d3.hierarchy({ children: keyshawnsPlayerData })
+        .sum(function (d) { return d.score || 1; });
+
+    const keyshawnsNodes = keyshawnsBubble(keyshawnsRoot).descendants();
+
+    const keyshawnsNode = keyshawnsSvg.selectAll(".node")
+        .data(keyshawnsNodes)
+        .enter()
+        .filter(d => !d.children)
+        .append("g")
+        .attr("class", "node")
+        .attr("transform", function (d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        });
+
+    keyshawnsNode.append("circle")
+        .attr("r", function (d) { return d.r; })
+        .style("fill", "lightgreen") 
+        .on("click", function (d) {
+            showKeyshawnPlayerImage(d.data.name);
+        });
+
+    keyshawnsNode.append("text")
+        .attr("dy", ".3em")
+        .style("text-anchor", "middle")
+        .text(function (d) { return d.data.name; })
+        .attr("class", "player-name")
+        .on("click", function (d) {
+            showKeyshawnPlayerImage(d.data.name);
+        });
+
+    function showKeyshawnPlayerImage(playerName) {
+        const keyshawnsModal = document.getElementById('keyshawns-player-modal');
+        const keyshawnsModalImage = document.getElementById('keyshawns-modal-image');
+        const keyshawnsModalStats = document.getElementById('keyshawns-modal-stats');
+        
+        // Retrieve player data from the map
+        const keyshawnsPlayerData = keyshawnsStatsMap.get(playerName);
+
+        if (keyshawnsPlayerDataPlayerData) {
+            const imagePath = keyshawnsImagePathMap[playerName];
+            keyshawnsModalImage.src = imagePath;
+            
+            
+            const formattedStats = Object.entries(keyshawnsPlayerData)
+                .map(([key, value]) => `${key}: ${value}`)
+                .join('\n');
+
+            // Display player stats
+            keyshawnsModalStats.textContent = `Stats:\n${formattedStats}`;
+        } else {
+            keyshawnsModalStats.textContent = 'Player data not available';
+        }
+
+        keyshawnsModal.style.display = 'block';
+
+        // Close modal on click outside the image
+        keyshawnsModal.onclick = function () {
+            keyshawnsModal.style.display = 'none';
+        };
+    }
+});
